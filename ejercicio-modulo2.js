be63d81a23f32241483d5470342d686249a15f7e
celerrodriguez <celesteerrodriguez@gmail.com>
2022-10-18 18:49:51 -0300

    ejercicio 2

---
+++ ejercicio-modulo2.js
@@ -0,0 +1,12 @@
+const datos = {
+    nombre: "Celeste",
+    edad: 36,
+    desarrollador: true,
+    "fecha-nac": new Date (1986, 6, 18),
+    "libro-favorito": {
+        titulo: "El principito",
+        autor: "Antoine de Saint-Exupéry",
+        fecha: 1946,
+        url: "https://www.audible.com/ep/espanol-3?asin=B00K0PP596&source_code=UHSGB3390223220164&device=d&&cvosrc=ppc%20content.googlegdn.17094801059&cvo_crid=595479660919&cvo_pid=137045959838&source_code=&gclid=Cj0KCQjwnbmaBhD-ARIsAGTPcfW1mkEBDoC4Mzmsee0DLahNx9N6dTjLkSxHlgorCcxHZ8oDai58WMsaAmUZEALw_wcB&gclsrc=aw.ds"
+    } 
+}
